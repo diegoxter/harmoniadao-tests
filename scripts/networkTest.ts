40520e78e05e38f3c9eefcd5de7dfca0b503a9a4
diegoxter <diegoxter1006@gmail.com>
2022-11-15 18:20:25 -0300

    Small fix

--- scripts/networkTest.ts
+++ scripts/networkTest.ts
@@ -3,10 +3,13 @@ const { ethers } = require('hardhat')
 describe('Accounts', function () {
     async function deployContractsFixture() {
         const [alice, bob, carol, david, erin] = await ethers.getSigners()
-        console.log('are the following: ')
-
+        
+        let count = 1 
         for (let thisUser of [alice, bob, carol, david, erin]) {
-            console.log(await thisUser.address)
+            console.log('Account number ' + count + ' address is ' + (await thisUser.address)
+                + ' with this amount of ether: ' + (await ethers.provider.getBalance(thisUser.address)))
+        
+            count++
         }
     }
     it('done', async function () {
